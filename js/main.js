9568264d79ebcdcdcae0d3a709ebb63690dbed77
Evan Massonnet <evanmassonnet@gmail.com>
2022-04-01 22:44:00 +0200

    init

--- js/main.js
+++ js/main.js
@@ -118,14 +118,14 @@ var mousewheelEvent = isFirefox ? "DOMMouseScroll" : "wheel";
 window.addEventListener(mousewheelEvent, parallaxScroll);
 document.addEventListener(mousewheelEvent, parallaxScroll)
 //on phone
-document.addEventListener("touchmove", parallaxScroll, false);
+/*document.addEventListener("touchmove", parallaxScroll, false);
 document.addEventListener("scroll", parallaxScroll, false);
-window.addEventListener("touchmove", parallaxScroll, false);
-window.addEventListener("scroll", parallaxScroll, false);
+window.addEventListener("touchmove", parallaxScroll, false);*/
+/*window.addEventListener("scroll", parallaxScroll, false);
 
 document.addEventListener("touchmove", parallaxScroll);
 document.addEventListener("scroll", parallaxScroll);
-window.addEventListener("touchmove", parallaxScroll);
+window.addEventListener("touchmove", parallaxScroll);*/
 window.addEventListener("scroll", parallaxScroll);
 
 
