43e174a14d32d8b500c6abcbf33718d40aa22821
Evan Massonnet <evanmassonnet@gmail.com>
2022-04-05 15:30:49 +0200

    init

--- js/sprite.js
+++ js/sprite.js
@@ -5,7 +5,7 @@ let frameSpeed = 0.1;
   var canvas_3 = document.getElementById( 'container_3' );
   
   
-  var Sprite = function(imgName, numColumns , numRows ) {
+  var Sprite = function(imgName, numColumns , numRows, position, scale) {
 
         console.log(imgName);
         this.img = new Image();
@@ -13,6 +13,8 @@ let frameSpeed = 0.1;
 
         this.numColumns = numColumns;
         this.numRows = numRows;
+        this.position = position;
+        this.scale = scale;
         
         this.frameWidth = this.img.width / numColumns;
         this.frameHeight = this.img.height / numRows;;
@@ -40,11 +42,11 @@ let frameSpeed = 0.1;
     draw: function( ctx ) {
         let column = this.currentFrame % this.numColumns;
         let row = Math.floor(this.currentFrame / this.numColumns);
-        ctx.drawImage(this.img, column * this.frameWidth, row * this.frameHeight, this.frameWidth, this.frameHeight, 100, 500, this.frameWidth, this.frameHeight);
+        ctx.drawImage(this.img, column * this.frameWidth, row * this.frameHeight, this.frameWidth, this.frameHeight, this.position.x, this.position.y, this.frameWidth * this.scale, this.frameHeight * this.scale);
       },
   };
   
-  var sprite;
+  var sprites = [];
   
   var sketch_3 = Sketch.create({
   
@@ -52,18 +54,28 @@ let frameSpeed = 0.1;
     container: canvas_3,
   
     setup: function() {
-      sprite = new Sprite('img/GreenRobotIdle.png', 4, 1);
+      
+      sprites.push(new Sprite('img/GreenRobotIdle.png', 4, 1, {x: vw/2-20, y:vh/2}, 1));
+      sprites.push(new Sprite('img/MainIdle.png', 4, 1, {x: vw/2+80, y:vh/2-100}, 0.6));
+      sprites.push(new Sprite('img/RedRobotIdle.png', 4, 1, {x: vw/2 +80, y:vh/2}, 1));
+      sprites.push(new Sprite('img/GunEnemyIdle.png', 4, 1, {x: vw/2 -20, y:vh/2-100}, 1.2));
+      sprites.push(new Sprite('img/GunIdle.png', 4, 1, {x: vw/2-120, y:vh/2-100}, 1.2));
+      sprites.push(new Sprite('img/OctoIdle.png', 11, 1, {x: vw/2-170, y:vh/2}, 0.3));
     },
   
     update: function() {
         var t;
         t = this.millis * 0.001;
-        sprite.update(t);
+        for(var i = 0; i < sprites.length; ++i){
+          sprites[i].update(t);
+        }
+        
     },
   
     draw: function() {
-
-      sprite.draw(this);
+      for(var i = 0; i < sprites.length; ++i){
+        sprites[i].draw(this);
+      }
     },
   
   });
