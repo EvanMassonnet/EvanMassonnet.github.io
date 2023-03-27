048bef626f5ad597227f7e293c98377085d63616
Guillaume-H-Skg <91275635+Guillaume-H-Skg@users.noreply.github.com>
2023-03-27 21:09:31 +0200

    Fix Asteroids

--- src/js/Asteroids.js
+++ src/js/Asteroids.js
@@ -1,4 +1,3 @@
-
 import * as THREE from '../js/three.js/build/three.module.js';
 import { GLTFLoader } from '../js/three.js/examples/jsm/loaders/GLTFLoader.js';
 
@@ -43,9 +42,18 @@ const createLights = () => {
 const positionAsteroids = () => {
     for (var i = 0; i < rock_count; i++) {
         //rocks[i].position.set((2 * Math.random() - 1) * 5, -1 + (2 * Math.random() - 1) * 0.5, (2 * Math.random() - 1) * 2);
-        rocks[i].position.set((2 * Math.random() - 1) * 20, -1 + (2 * Math.random() - 1) * 0.5, (2 * Math.random() - 1) * 2);
+        rocks[i].position.set((2 * Math.random() - 1) * 30, -1 + (2 * Math.random() - 1) * 1, (2 * Math.random() - 1) * 3);
+        
+        if(Math.abs(rocks[rocks.length-1].position.x - rocks[i].position.x) <= 20) {
+            console.log("ada",Math.abs(rocks[rocks.length-1].position.x - rocks[i].position.x));
+            rocks[i].position.set((2 * Math.random() - 1) * 20, -1 + (2 * Math.random() - 1) * 1, (2 * Math.random() - 1) * 3);
+            console.log(i,rocks[i].position);
+        }
+
         //rocks[i].scale.set(0.3 + Math.random() * 0.05, 0.3 + Math.random() * 0.05, 0.3 + Math.random() * 0.05);
-        rocks[i].scale.set(0.5 + Math.random() * 0.05, 0.5 + Math.random() * 0.05, 0.5 + Math.random() * 0.05);
+        rocks[i].scale.set(0.3 + Math.random() * 0.5, 0.2 + Math.random() * 0.5, 0.3 + Math.random() * 0.5);
+        //rocks[i].scale.set(0.3 +  0.5, 0.2 +  0.5, 0.3 + 0.5);
+
     }
 };
 
@@ -59,14 +67,14 @@ function init() {
 
     camera = new THREE.PerspectiveCamera(45, window.innerWidth / 200, 0.25, 20);
     //camera = new THREE.OrthographicCamera( -window.innerWidth, window.innerWidth, -window.innerHeight, window.innerHeight, 0.01, 200 );
-    camera.position.set(0, 0, 5);
+    camera.position.set(0, 0, 8);
 
     scene = new THREE.Scene();
 
     const loader = new GLTFLoader().setPath('../dist/models/');
     loader.load('rock.gltf', function (gltf) {
         default_rock = gltf.scene;
-        default_rock.scale.set(0.3, 0.3, 0.3);
+        default_rock.scale.set(1, 1, 1);
         default_rock.name = 'rock';
         //rocks.push(default_rock);
         //scene.add(default_rock);
@@ -78,7 +86,8 @@ function init() {
             rocks.push(rock);
             rocks_x.push(Math.random() * 0.01);
             rocks_y.push(Math.random() * 0.01);
-            rocks_speed.push(0.001 + Math.random() * 0.0012);
+            // rocks_speed.push(0.001 + Math.random() * 0.0012);
+            rocks_speed.push(0.005 + Math.random() * 0.0012);
             scene.add(rock);
         }
 
@@ -90,7 +99,7 @@ function init() {
     renderer = new THREE.WebGLRenderer({ antialias: true }, { alpha: true });
     renderer.setClearColor(0x000000, 0);
     renderer.setPixelRatio(window.devicePixelRatio);
-    renderer.setSize(window.innerWidth, 200);
+    renderer.setSize(window.innerWidth, 300);
     renderer.toneMapping = THREE.ACESFilmicToneMapping;
     renderer.toneMappingExposure = 1;
     renderer.setAnimationLoop(animation);
@@ -104,11 +113,11 @@ function init() {
 
 function onWindowResize() {
 
-    camera.aspect = window.innerWidth / 200;
+    camera.aspect = window.innerWidth / 300;
     camera.updateProjectionMatrix();
     positionAsteroids();
 
-    renderer.setSize(window.innerWidth, 200);
+    renderer.setSize(window.innerWidth, 300);
 
     render();
 
@@ -128,8 +137,11 @@ function animation(time) {
 
         rocks[i].position.x += rocks_speed[i];
 
-        if (rocks[i].position.x > window.innerWidth / 400) {
-            rocks[i].position.x = -window.innerWidth / 400;
+        if (rocks[i].position.x > window.innerWidth / 50 ) {
+            console.log(window.innerWidth);
+            console.log(window.innerWidth / 50);
+            rocks[i].position.x = -40;
+            console.log(rocks[i].position.x);
         }
     }
 
