6ac28a0d2005ccebe00fe2b33a7196aacb437c7a
Guillaume-H-Skg <91275635+Guillaume-H-Skg@users.noreply.github.com>
2023-03-30 21:39:50 +0200

    Fix header unwanted padding

--- src/js/AstronauteHead.js
+++ src/js/AstronauteHead.js
@@ -44,7 +44,7 @@ function init() {
 
     const container = document.getElementsByClassName('timeline_canvas_left')[0];
 
-    camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.25, 20);
+    camera = new THREE.PerspectiveCamera(45, 1, 0.25, 20);
     camera.position.set(0, 0, 5);
 
     scene = new THREE.Scene();
